be057719112ecc16c51ab5b0adcf636f2b6d3b6c
maximedaraize <maxime.daraize@gmail.com>
2020-04-13 17:29:23 -0400

    reset inital code created by vu app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Colors></Colors>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Colors from "@/components/Colors.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Colors
   }
 };
 </script>
